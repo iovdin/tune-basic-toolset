ba1fc119fab10f320da08a207a8857b110e483b3
Ilya Ovdin <iovdin@gmail.com>
2026-04-01 11:54:32 +0200

    feat: Add tune-sdk/man integration
    Integrates tune-sdk/man with the basic toolset, allowing it to manage and document tools.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const { tools } = require('tune-fs')
+const man = require('tune-sdk/man')
 
-// TODO js tool packages include
-module.exports = (...opts) => 
-  tools({ ...opts, path: __dirname })
+module.exports = (...opts) => {
+  man.addPackage(__dirname)
+  return tools({ ...opts, path: __dirname })
+}
 
